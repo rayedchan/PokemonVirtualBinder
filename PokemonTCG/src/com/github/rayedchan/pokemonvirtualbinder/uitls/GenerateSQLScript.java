7a0f8c4c11e733a5be6a59ece41ebe2244f29e0a
rayedchan <rayedchan@hotmail.com>
2015-06-28 15:36:10 -0400

    Update

---
+++ PokemonTCG/src/com/github/rayedchan/pokemonvirtualbinder/uitls/GenerateSQLScript.java
@@ -0,0 +1,50 @@
+package com.github.rayedchan.pokemonvirtualbinder.uitls;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rayedchan
+ */
+public class GenerateSQLScript
+{
+    private static final Logger LOGGER =  Logger.getLogger(GenerateSQLScript.class.getName());
+    
+    public static void main(String[] args)
+    {
+        PrintWriter writer = null;
+                
+        try
+        {
+            
+            
+            
+            
+            // Get current working directory of project
+            String currentDirectory = System.getProperty("user.dir");
+            System.out.println("Working Directory = " + currentDirectory);
+        
+            String fileName = "sqlscripts/Pokedex.sql"; // Relative to project
+            writer = new PrintWriter(fileName, "UTF-8");
+            
+            writer.println("Hello");
+        } 
+        
+        catch (FileNotFoundException | UnsupportedEncodingException ex)
+        {
+           LOGGER.log(Level.SEVERE, null, ex);
+        }
+        
+        finally
+        {
+            if(writer != null)
+            {
+                writer.close();
+            }
+        }
+    }
+}
